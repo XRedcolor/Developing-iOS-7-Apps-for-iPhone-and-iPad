e8135ade4f603946cd2a1c21ce380fc7b9da92cc
xredcolor <xredcolor@163.com>
2015-07-24 16:10:59 +0800

    new model Card

---
+++ Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,26 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by XRedcolor on 15/7/24.
+//  Copyright (c) 2015年 XRedcolor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+/**
+ *  card 内容
+ */
+@property (nonatomic, strong) NSString *contents;
+/**
+ *  card 是否选择
+ */
+@property (nonatomic, getter=isChosen) BOOL chosen;
+/**
+ *  card 是否匹配
+ */
+@property (nonatomic, getter=isMatched) BOOL matched;
+
+@end
---
+++ Matchismo/Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by XRedcolor on 15/7/24.
+//  Copyright (c) 2015年 XRedcolor. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
