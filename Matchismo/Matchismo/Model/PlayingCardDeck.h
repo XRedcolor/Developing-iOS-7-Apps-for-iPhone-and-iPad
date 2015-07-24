63ec638ac7effc22d9e549ccc881070b471af18d
xredcolor <xredcolor@163.com>
2015-07-24 17:33:05 +0800

    new model PlayingCardDeck

---
+++ Matchismo/Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by XRedcolor on 15/7/24.
+//  Copyright (c) 2015年 XRedcolor. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
